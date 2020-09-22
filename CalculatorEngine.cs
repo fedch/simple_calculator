0dd70d1d436d8b41379ff9eccbd906c3c4f6c5e9
Elena Fedchenko <fedchenko.yelena@gmail.com>
2020-09-22 22:00:16 +1200

    First commit

---
+++ CalculatorEngine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleCalculatorPro
+{
+    class CalculatorEngine
+    {
+        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+            // Declare a variable:
+            double result;
+
+            switch (argOperation.ToLower())
+            {
+                case "add":
+                case "+":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+
+                case "subtract":
+                case "-":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+
+                case "multiply":
+                case "*":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+
+                case "divide":
+                case "/":
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+
+                default:
+                    throw new InvalidOperationException("The operation is not recognised.");
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ InputConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleCalculatorPro
+{
+    public class InputConverter
+    {
+        // Create a method to make sure input is numeric
+        public double ConvertInputToNumeric(string argTextInput)
+        {
+            // Declare a variable:
+            double convertedNumber;
+            // Check if the input was numeric and throw an exception if not:
+            if (!double.TryParse(argTextInput, out convertedNumber)) throw new ArgumentException("Expected a numeric value.\r\n");
+            return convertedNumber;
+        }
+    }
+}
